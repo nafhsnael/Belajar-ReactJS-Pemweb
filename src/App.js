2064eb87b0aa342f16582a5cf1cf06c794fa4c52
nafhs.nael <nailanafhisa66@gmail.com>
2026-03-28 21:04:26 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="app-container">
+            <header className="app-header">
+                <h1>React JS Project</h1>
+                <h1>KELOMPOK 2</h1>
+
+                <p>Nama: Nafhisa Nailah HUsnah</p>
+                <p>NIM: 253140707111072</p>
+                <br></br>
+
+                <p>Nama: Serli Maharani Putri Yustina</p>
+                <p>NIM: 253140707111084</p>
+                <br></br>
+                ""
+                <p>Nama: Firyal Zalfaa Aulia</p>
+                <p>NIM: 253140707111070</p>
+                <br></br>
+
+                <p>Nama: Kasiva Imtiyas Zaidah Iftinan</p>
+                <p>NIM: 253140707111085</p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
